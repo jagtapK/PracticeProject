9e30dca072a2c55dc747e42ab609aaed39ae2b0c
jagtapK <jagtapkajal54@gmail.com>
2025-07-31 23:06:12 +0530

    get city using @requestParam Annotation

--- PracticeProject/src/main/java/com/spring/PracticeProject/controller/MyController.java
+++ PracticeProject/src/main/java/com/spring/PracticeProject/controller/MyController.java
@@ -4,10 +4,7 @@ import com.spring.PracticeProject.entity.Student;
 import com.spring.PracticeProject.repo.StudentRepo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.lang.model.element.Name;
 import java.util.ArrayList;
@@ -93,6 +90,28 @@ public ResponseEntity<List<Student>> getAllStudent(){
     return new ResponseEntity<>(sortedList, HttpStatus.OK);
     }
 
+
+    @GetMapping("/getStudent")
+    public ResponseEntity<List<Student>> getStudent(@RequestParam(required = false) String city){
+
+        System.err.println("City is: " + city);
+    List<Student> s = StudentRepo.getAllStudent();
+
+    List<Student> sortedList = new ArrayList<>();
+
+        if (city != null) {
+
+            for(Student s1 : s){
+               if(city.equalsIgnoreCase(s1.getCity())){
+                   sortedList.add(s1);
+               }
+            }
+            return  new ResponseEntity<>(sortedList, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(s, HttpStatus.OK);
+        }
+    }
 
 
 }
